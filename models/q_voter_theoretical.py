03c93d012110c51d36e5fe58f3b17fbf4fb314b8
Ryszard <ryszardrolik@gmail.com>
2022-05-25 23:34:21 +0200

    figure 2 theoretically reproduced
    Now it is time to put in the MC results

--- models/q_voter_theoretical.py
+++ models/q_voter_theoretical.py
@@ -4,18 +4,39 @@ from scipy import integrate
 import pandas as pd
 
 def c_down(c):
+    """equation for c down pointing arrow
+    """    
     return 1 - c
 
 def c_up(c):
+    """equation for c up pointing arrow
+    """ 
     return c
 
 def theta_down(b, c):
+    """equation for theta down pointing arrow
+    """ 
     return b / (2 * (1 - c))
     
 def theta_up(b, c):
+    """equation for theta up pointing arrow
+    """ 
     return b / ((2 * c))
 
-def PA_differential_equations(t, X, p, k, q_a, q_c):
+def PA_differential_equations(X, p, k, q_a, q_c):
+    """Differential equations system for PA model. The first equation is the time evolution of proportion of positive opinion
+    and the second is the time evolution of proportion of active bonds (bonds between different opinions).
+
+    Args:
+        X (list): two element list of c and b values
+        p (float): probability of anticonformity, p > 0, p < 1
+        k (int): size of the network
+        q_a (int): size of the anticonformity group
+        q_c (int): size of the conformity group
+
+    Returns:
+        array: two element array of c and b values after evolution in time step
+    """    
     c = X[0]
     b = X[1]
     return np.array([ (1 - p) * (c_down(c) * theta_down(b, c) ** q_c - c_up(c) * theta_up(b ,c) ** q_c) + p * (c_down(c) * ( 1 - theta_down(b, c)) ** q_a - c_up(c) * (1 - theta_up(b, c)) ** q_a) ,
@@ -50,3 +71,8 @@ def PA_unstable_fixed_points(C, k, q_a, q_c, IC, interpolate = False, lower_inte
         a = a.interpolate(method='polynomial', order = 2)
         Stable_states = a.to_numpy()
     return Stable_states
+
+def MFA_equation(c, q_a, q_c):
+    X = (1 - c) * c ** q_c - c * (1 - c) ** q_c
+    Y = (1 - c) ** (q_a + 1) - c ** (q_a + 1)
+    return X / (X - Y)
