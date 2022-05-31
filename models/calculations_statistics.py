a6b82e049c81e6d986200674a619b6f398d0b13e
bognaj <249811@student.pwr.edu.pl>
2022-05-31 18:45:54 +0200

    all the files ready to be plotted (fig 2 & 3)

--- models/calculations_statistics.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data150 = pd.read_csv('c.txt', sep=" ", names=["p", "c"])
-print(data150.head())
-
-data50 = pd.read_csv('c50.txt', sep=" ", names=["p", "c"])
-print(data50.head())
